2b903a239b263abeb2c23b537aaef7065c1aaea6
sani-1023 <sanitalukder1998@gmail.com>
2022-08-28 00:37:08 +0600

    transaction is coming.....

--- src/component/Cart/Payment.js
+++ src/component/Cart/Payment.js
@@ -9,25 +9,66 @@ import CreditCardIcon from "@material-ui/icons/CreditCard";
 import EventIcon from "@material-ui/icons/Event";
 import VpnKeyIcon from "@material-ui/icons/VpnKey";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Payment = () => {
   const orderInfo = JSON.parse(sessionStorage.getItem("orderInfo"));
   const productInfo = JSON.parse(localStorage.getItem("cartItems"));
+  const payBtn = useRef(null);
+  const navigate = useNavigate();
+
+  const [bankAccount, setBankAccount] = useState("");
+  const [secretKey, setSecretKey] = useState("");
+
+  // setTotalPrice(orderInfo.totalPrice);
+  // const [loginPassword, setLoginPassword] = useState("");
+
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    axios.get(`api/v1/me`).then((res) => {
+      setUser(res.data.user);
+
+      // console.log(user)
+    });
+  }, []);
 
   const totalPrice = orderInfo.totalPrice;
 
-  console.log("hi",productInfo);
-  console.log("hi",orderInfo);
+  //console.log(secret);
 
-  const payBtn = useRef(null);
+  const order = productInfo.map((obj) => ({
+    productId: obj.product,
+    productName: obj.name,
+    quantity: obj.quantity,
+
+  }));
 
  
+
+  const transactionInfo = {
+    bankAccount:bankAccount,
+    totalAmount:totalPrice,
+    secretKey:secretKey,
+    userId:user._id,
+    order:order
+  }
+
+ console.log(transactionInfo)
+  
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
+    // const { bloodGroup, bagOfBlood, location, contact } = post;
+    // console.log(post);
 
+    axios.post(`api/v1/usertransaction`, transactionInfo).then(async(res) => {
+      alert("Order is confirmed...Payment will be deduced soon");
+      navigate("/")
 
+    }).catch((e) => {
+      alert(e.response.data.message);
+  });
   };
 
   return (
@@ -39,7 +80,11 @@ const Payment = () => {
           <Typography>Card Info</Typography>
           <div>
             <CreditCardIcon />
-            <input className="paymentInput" placeholder="Bank-account"/>
+            <input
+              className="paymentInput"
+              placeholder="Bank-account"
+              onChange={(e) => setBankAccount(e.target.value)}
+            />
           </div>
           <div>
             <EventIcon />
@@ -47,7 +92,12 @@ const Payment = () => {
           </div>
           <div>
             <VpnKeyIcon />
-            <input className="paymentInput" placeholder="Secret key" type = "password" />
+            <input
+              className="paymentInput"
+              placeholder="Secret key"
+              type="password"
+              onChange={(e) => setSecretKey(e.target.value)}
+            />
           </div>
 
           <input
