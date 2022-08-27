5e4aaf5a4fd5a4b3a7c30904da05238acedfe73c
Abhishek <abhishekdey538@gmail.com>
2022-08-28 04:37:59 +0600

    image file added

--- src/component/User/profile.js
+++ src/component/User/profile.js
@@ -1,84 +1,127 @@
-import React, { useState, useEffect } from "react";
-import "./profile.css";
-import { useSelector, useDispatch } from "react-redux";
-import axios from "axios";
-import profile  from "../../images/profile.png"
-import ProfileNavBar from "../User/ProfileNav";
-
+import React, { useState, useEffect } from 'react';
+import './profile.css';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
+import profile from '../../images/profile.png';
+import ProfileNavBar from '../User/ProfileNav';
 
 const Profile = () => {
-  //const { user, loading, isAuthenticated } = useSelector((state) => state.user);
+	//const { user, loading, isAuthenticated } = useSelector((state) => state.user);
 
-  const [user, setUser] = useState({});
-  useEffect(() => {
-    axios.get(`api/v1/me`).then((res) => {
-      setUser(res.data.user);
+	const [user, setUser] = useState({});
 
-      // console.log(user)
-    });
-  }, []);
+	const [file, setFile] = useState(null);
 
-  return (
+	const handleFileChange = (event) => {
+		setFile(event.target.files);
+		//console.log("hi",file);
+	};
 
-    <>
-    <ProfileNavBar/>
-    <div className="main">
-      <div className="container bootstrap snippets bootdey">
-        <div className="row">
-          <div className="profile-nav col-md-3">
-            <div className="panel">
-              <div className="user-heading round">
-                <a href="#">
-                  <img
-                    src={profile}
-                    alt=""
-                  />
-                </a>
-                <h1>{user.name}</h1>
-                <p>{user.email}</p>
-              </div>
-            </div>
-          </div>
-          <div className="profile-info col-md-9">
-            <div className="panel">
-              <div className="bio-graph-heading">Your Profile</div>
-              <div className="panel-body bio-graph-info">
-                <h1>Basic Information</h1>
-                <div className="row">
-                  <div className="bio-row">
-                    <p>
-                      <span>Name </span>: {user.name}
-                    </p>
-                  </div>
-                  <div className="bio-row">
-                    <p>
-                      <span>Country </span>: Bangladesh
-                    </p>
-                  </div>
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const data = new FormData();
+		data.append('file', file[0]);
 
-                  <div className="bio-row">
-                    <p>
-                      <span>Email </span>: {user.email}
-                    </p>
-                  </div>
-                  <div className="bio-row">
-                    <p>
-                      <span>Mobile </span>: +88 {user.phone}
-                    </p>
-                  </div>
-                  <div className="bio-row">
-                    <p>
-                      <span>Bank Account </span>: {user.bankAccount}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="your-order">your recent orders</div>
-      {/* <div className="order-container">
+    console.log(data)
+
+		axios
+			.post(`api/v1/upload`, data)
+			.then(async (res) => {
+				alert('upload is confirmed...');
+			})
+			.catch((e) => {
+				alert(e);
+			});
+	};
+
+	useEffect(() => {
+		axios.get(`api/v1/me`).then((res) => {
+			setUser(res.data.user);
+
+			// console.log(user)
+		});
+	}, []);
+
+	return (
+		<>
+			<ProfileNavBar />
+			<div className='main'>
+				<div className='container bootstrap snippets bootdey'>
+					<div className='row'>
+						<div className='profile-nav col-md-3'>
+							<div className='panel'>
+								<div className='user-heading round'>
+									<a href='#'>
+										<img src={profile} alt='' />
+									</a>
+									<div>
+										<form>
+											<div className='form-group'>
+												<label htmlFor='file'>Upload File:</label>
+												<input
+													className='form-control-file mb-3'
+													type='file'
+													id='file'
+													accept='.png'
+													multiple
+													onChange={handleFileChange}
+												/>
+
+												<button
+													className='btn btn-primary mt-3'
+													onClick={handleSubmit}>
+													Upload
+												</button>
+											</div>
+										</form>
+
+										{/* Display Image Here */}
+									</div>
+									<h1>{user.name}</h1>
+									<p>{user.email}</p>
+								</div>
+							</div>
+						</div>
+						<div className='profile-info col-md-9'>
+							<div className='panel'>
+								<div className='bio-graph-heading'>Your Profile</div>
+								<div className='panel-body bio-graph-info'>
+									<h1>Basic Information</h1>
+									<div className='row'>
+										<div className='bio-row'>
+											<p>
+												<span>Name </span>: {user.name}
+											</p>
+										</div>
+										<div className='bio-row'>
+											<p>
+												<span>Country </span>: Bangladesh
+											</p>
+										</div>
+
+										<div className='bio-row'>
+											<p>
+												<span>Email </span>: {user.email}
+											</p>
+										</div>
+										<div className='bio-row'>
+											<p>
+												<span>Mobile </span>: +88 {user.phone}
+											</p>
+										</div>
+										<div className='bio-row'>
+											<p>
+												<span>Bank Account </span>: {user.bankAccount}
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='your-order'>your recent orders</div>
+				{/* <div className="order-container">
                 
                      <p><span> Order Id : </span>    sjbkjfswjfobejnofjo</p>
                      <p><span> Product name : </span>   product 1</p>
@@ -87,33 +130,33 @@ const Profile = () => {
                      <p><span> Date : </span>   Today</p>
                
             </div> */}
-      <div class="card">
-        <div class="container">
-          <h4>
-            <b>Order Id:</b>
-            <b> lnsdhlknldknlks</b>
-          </h4>
-          <h4>
-            <b>Product Name:</b>
-            <b> product1</b>
-          </h4>
-          <h4>
-            <b>Quantity:</b>
-            <b> 5</b>
-          </h4>
-          <h4>
-            <b>Total Price:</b>
-            <b> 500/=</b>
-          </h4>
-          <h4>
-            <b>Date:</b>
-            <b> Today</b>
-          </h4>
-        </div>
-      </div>
-    </div>
-    </>
-  );
+				<div class='card'>
+					<div class='container'>
+						<h4>
+							<b>Order Id:</b>
+							<b> lnsdhlknldknlks</b>
+						</h4>
+						<h4>
+							<b>Product Name:</b>
+							<b> product1</b>
+						</h4>
+						<h4>
+							<b>Quantity:</b>
+							<b> 5</b>
+						</h4>
+						<h4>
+							<b>Total Price:</b>
+							<b> 500/=</b>
+						</h4>
+						<h4>
+							<b>Date:</b>
+							<b> Today</b>
+						</h4>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Profile;
